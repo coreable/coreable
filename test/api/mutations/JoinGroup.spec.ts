76a299861e94b180cb869437ebe4a140f8bc49fd
svnty <jakewalklate@gmail.com>
2020-03-27 13:08:48 +1100

    make joinGroup test more reliable

--- test/api/mutations/JoinGroup.spec.ts
+++ test/api/mutations/JoinGroup.spec.ts
@@ -10,10 +10,13 @@ chai.use(chaiHttp);
 describe('JoinGroup Mutation [api/mutations/JoinGroup.ts]', () => { 
   let sessionToken: string;
   let user: any;
+  let group: any;
+  let user2: any;
+  let group2: any;
 
   before(async() => {
     await server.load;
-    await Group.create({
+    group = await Group.create({
       groupName: 'unitTest',
       groupLeaderID: 1,
       inviteCode: '123',
@@ -43,11 +46,17 @@ describe('JoinGroup Mutation [api/mutations/JoinGroup.ts]', () => {
     });
     user = res.body.data.register.result.user;
     sessionToken = res.body.data.register.result.session.token;
-    await Group.create({
+    group2 = await Group.create({
       groupName: 'unitTest2',
       groupLeaderID: res.body.data.register.result.user.userID,
       inviteCode: '1234',
       industryID: 1,
+    });
+    user2 = await User.create({
+      firstName: 'unit',
+      lastName: 'test',
+      password: 'unittest',
+      email: 'unit2@test2.com'
     });
     return;
   });
@@ -196,10 +205,35 @@ describe('JoinGroup Mutation [api/mutations/JoinGroup.ts]', () => {
     return expect(res.body.data.joinGroup.result).to.not.exist;
   });
 
+  it('should deny an authenticated user making another user join a group with the correct invite code', async() => {
+    let res = await chai.request(server).post('/graphQL').set('JWT', sessionToken).send({
+      query:
+      `mutation {
+        joinGroup(userID: ${user2.userID}, inviteCode: "123") {
+          result {
+            user {
+              userID
+            }
+            group{
+              groupName
+            }
+          }
+          errors {
+            code
+            path
+            message
+          }
+        }
+      }`
+    });
+    return expect(res.body.data.joinGroup.errors).to.exist;
+  });
+
   after(async() => {
     await Group.destroy({ where: { 'groupName': 'unitTest' }});
     await Group.destroy({ where: { 'groupName': 'unitTest2' }});
     await User.destroy({ where: { 'email': 'unit@test.com' }});
+    await User.destroy({ where: { 'email': 'unit2@test2.com' }});
   });
 
 });
