67dbe6c82538bfe17c377540b8820d6b3e05c3ea
svnty <jakewalklate@gmail.com>
2020-04-07 18:46:06 +1000

    add license per file

--- api/models/Team.ts
+++ api/models/Team.ts
@@ -1,3 +1,17 @@
+/*
+===========================================================================
+Copyright (C) 2020 Coreable
+This file is part of Coreable's source code.
+Corables source code is free software; you can redistribute it
+and/or modify it under the terms of the End-user license agreement.
+Coreable's source code is distributed in the hope that it will be
+useful, but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.
+You should have received a copy of the license along with the 
+Coreable source code.
+===========================================================================
+*/ 
+
 import { Model, DataTypes, Sequelize } from 'sequelize';
 import { User } from './User';
 import { Subject } from './Subject';
--- api/server.ts
+++ api/server.ts
@@ -1,3 +1,24 @@
+/*
+===========================================================================
+Copyright (C) 2020 Coreable
+This file is part of Coreable's source code.
+Corables source code is free software; you can redistribute it
+and/or modify it under the terms of the End-user license agreement.
+Coreable's source code is distributed in the hope that it will be
+useful, but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.
+You should have received a copy of the license along with the 
+Coreable source code.
+===========================================================================
+*/ 
+
+/**
+ * Author: Jake Walklate <jakewalklate@gmail.com> (svnty)
+ * Date: Tue 7 Apr 2020
+ * Purpose: The application access layer to perform create, read, update and delete database
+ * operations for Coreable's web and mobile applications.
+ */
+
 import { server } from './lib/startup';
 import { app } from './lib/express';
 import { createServer, Server } from 'http';
