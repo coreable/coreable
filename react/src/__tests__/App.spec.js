4bba81d9352891d622edee0b473af26e46875e42
Hs <hanslee358@gmail.com>
2020-04-22 20:44:26 +1000

    update

--- react/src/__tests__/App.spec.js
+++
@@ -1,7 +0,0 @@
-// import React from 'react';
-// import { render, getAllByText } from '@testing-library/react';
-// import App from '../App';
-
-test('fake test', () => {
-  expect(true).toBeTruthy();
-});
