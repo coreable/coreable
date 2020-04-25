15bc1815711166e60c6b9b1e3a7638f5ba881a20
Hs <hanslee358@gmail.com>
2020-04-25 10:38:57 +1000

    setup router changed to home

--- react/src/components/LandingPage/Register/Register.js
+++ react/src/components/LandingPage/Register/Register.js
@@ -15,20 +15,20 @@ Coreable source code.
 // TODO: Disable register button while waiting for submit
 
 import React, { Component } from "react";
-import './Register.scss';
-import { Link, Redirect } from 'react-router-dom';
+import "./Register.scss";
+import { Link, Redirect } from "react-router-dom";
 
-import { Mutation } from 'react-apollo';
-import { JWT, USER_NAME, USERID } from '../../../constants';
-import { SIGNUP_MUTATION } from '../../../apollo/mutations';
+import { Mutation } from "react-apollo";
+import { JWT, USER_NAME, USERID } from "../../../constants";
+import { SIGNUP_MUTATION } from "../../../apollo/mutations";
 
 import {
   Typography,
   Container,
   Button,
   TextField,
-  FormControl
-} from '@material-ui/core';
+  FormControl,
+} from "@material-ui/core";
 
 class Register extends Component {
   constructor() {
@@ -46,8 +46,8 @@ class Register extends Component {
         email: false,
         password: false,
         firstname: false,
-        lastname: false
-      }
+        lastname: false,
+      },
     };
   }
 
@@ -58,7 +58,12 @@ class Register extends Component {
   }
 
   errors() {
-    return this.validate(this.state.email, this.state.password, this.state.firstname, this.state.lastname);
+    return this.validate(
+      this.state.email,
+      this.state.password,
+      this.state.firstname,
+      this.state.lastname
+    );
   }
 
   validate(email, password, firstname, lastname) {
@@ -66,26 +71,26 @@ class Register extends Component {
       email: email.length === 0 || !email.includes("@"),
       password: password.length <= 4,
       firstname: firstname.length <= 1,
-      lastname: lastname.length <= 1
+      lastname: lastname.length <= 1,
     };
-  };
+  }
 
   handleChange = (evt) => {
     this.setState({ [evt.target.name]: evt.target.value });
   };
 
-  handleBlur = field => (evt) => {
+  handleBlur = (field) => (evt) => {
     this.setState({
-      touched: { ...this.state.touched, [field]: true }
+      touched: { ...this.state.touched, [field]: true },
     });
   };
 
   getColour = (field) => {
     if (this.state.touched[field] && !this.errors()[field]) {
-      return '#e8f0fe';
+      return "#e8f0fe";
     }
-    return '#f7f9fc';
-  }
+    return "#f7f9fc";
+  };
 
   handleSubmit = (evt) => {
     if (!this.isDisabled) {
@@ -98,9 +103,9 @@ class Register extends Component {
     const hasError = this.errors()[field];
     const shouldShow = this.state.touched[field];
     return hasError ? shouldShow : false;
-  };
+  }
 
-  isDisabled = () => Object.keys(this.errors()).some(x => this.errors()[x]);
+  isDisabled = () => Object.keys(this.errors()).some((x) => this.errors()[x]);
 
   _success = (data) => {
     this.setState({ loading: false });
@@ -110,36 +115,53 @@ class Register extends Component {
       this._saveUserData({ token, firstName, _id });
       this.props.history.push(`/setup`);
     } catch (err) {
-      alert('Invalid signup');
+      alert("Invalid signup");
     }
-  }
+  };
 
   _error = (err) => {
     console.error(err);
-  }
+  };
 
   _saveUserData = ({ token, firstName, _id }) => {
     localStorage.setItem(JWT, token);
     localStorage.setItem(USER_NAME, firstName);
     localStorage.setItem(USERID, _id);
-  }
+  };
 
   render() {
     if (this.state.loggedIn) {
-      return (<Redirect to="/setup"></Redirect>);
+      return <Redirect to="/home"></Redirect>;
     }
 
     let { email, password, firstname, lastname } = this.state;
     return (
-      <Container maxWidth="xl" style={{ height: '95.25vh' }} className="sign-up-container">
+      <Container
+        maxWidth="xl"
+        style={{ height: "95.25vh" }}
+        className="sign-up-container"
+      >
         <Container maxWidth="md">
-          <Typography variant="h3" component="h1" style={{ fontWeight: 'bold', marginTop: '48pt', textAlign: 'left', color: '#000' }}>
+          <Typography
+            variant="h3"
+            component="h1"
+            style={{
+              fontWeight: "bold",
+              marginTop: "48pt",
+              textAlign: "left",
+              color: "#000",
+            }}
+          >
             Welcome,
           </Typography>
-          <Typography variant="h3" component="h1" style={{ textAlign: 'left', color: '#707070' }}>
+          <Typography
+            variant="h3"
+            component="h1"
+            style={{ textAlign: "left", color: "#707070" }}
+          >
             sign up to continue
           </Typography>
-          <FormControl style={{ marginTop: '16pt' }}>
+          <FormControl style={{ marginTop: "16pt" }}>
             <TextField
               label="First Name"
               placeholder="Enter first name"
@@ -156,7 +178,10 @@ class Register extends Component {
               required
               onChange={this.handleChange}
               onBlur={this.handleBlur("firstname")}
-              style={{ marginTop: '8pt', backgroundColor: this.getColour("firstname") }}
+              style={{
+                marginTop: "8pt",
+                backgroundColor: this.getColour("firstname"),
+              }}
             />
             <TextField
               label="Last Name"
@@ -174,7 +199,10 @@ class Register extends Component {
               required
               onChange={this.handleChange}
               onBlur={this.handleBlur("lastname")}
-              style={{ marginTop: '8pt', backgroundColor: this.getColour("lastname") }}
+              style={{
+                marginTop: "8pt",
+                backgroundColor: this.getColour("lastname"),
+              }}
             />
             <TextField
               label="Email"
@@ -192,7 +220,10 @@ class Register extends Component {
               required
               onChange={this.handleChange}
               onBlur={this.handleBlur("email")}
-              style={{ marginTop: '8pt', backgroundColor: this.getColour("email") }}
+              style={{
+                marginTop: "8pt",
+                backgroundColor: this.getColour("email"),
+              }}
             />
             <TextField
               label="Password"
@@ -210,33 +241,41 @@ class Register extends Component {
               required
               onChange={this.handleChange}
               onBlur={this.handleBlur("password")}
-              style={{ marginTop: '8pt', marginBottom: '8pt', backgroundColor: this.getColour("password") }}
+              style={{
+                marginTop: "8pt",
+                marginBottom: "8pt",
+                backgroundColor: this.getColour("password"),
+              }}
             />
             <Mutation
               mutation={SIGNUP_MUTATION}
               variables={{ email, password, firstname, lastname }}
-              onCompleted={data => this._success(data)}
-              onError={err => this._error(err)}
+              onCompleted={(data) => this._success(data)}
+              onError={(err) => this._error(err)}
             >
               {(mutation, { _, loading, __ }) => {
                 return (
-                  <Button className="btn-sign-up" disabled={this.isDisabled() && !this.state.loading} onClick={mutation}>
+                  <Button
+                    className="btn-sign-up"
+                    disabled={this.isDisabled() && !this.state.loading}
+                    onClick={mutation}
+                  >
                     Sign up
                   </Button>
                 );
               }}
             </Mutation>
-            <Link to="/forgot" style={{ marginTop: '8pt', textDecoration: 'none' }}>
-              <Button color="primary">
-                Forgot password
-                </Button>
+            <Link
+              to="/forgot"
+              style={{ marginTop: "8pt", textDecoration: "none" }}
+            >
+              <Button color="primary">Forgot password</Button>
             </Link>
           </FormControl>
         </Container>
       </Container>
     );
   }
-
 }
 
 export default Register;
