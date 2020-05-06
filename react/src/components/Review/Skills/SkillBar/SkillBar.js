b45af92369b51acf2354070261e0e4cfca291d8c
Hs <hanslee358@gmail.com>
2020-05-06 18:14:00 +1000

    skills css

--- react/src/components/Review/Skills/SkillBar/SkillBar.js
+++ react/src/components/Review/Skills/SkillBar/SkillBar.js
@@ -1,15 +1,42 @@
 import React from "react";
 import { Typography } from "@material-ui/core";
 
+import "./SkillBar.scss";
+
 export default function SkillBar(props) {
   const { field, value } = props.values;
 
   return (
     <div>
-      <Typography>{field + value}</Typography>
-      <div
-        style={{ width: `${value}%`, height: "10px", background: "pink" }}
-      ></div>
+      <Typography
+        variant="h6"
+        // component="h1"
+        style={{
+          fontWeight: "bold",
+          textAlign: "left",
+          color: "black",
+          paddingTop: "10px",
+          paddingBottom: "5px",
+        }}
+      >
+        {field}
+      </Typography>
+      <div style={{ position: "relative" }}>
+        <div
+          style={{
+            width: `${value}%`,
+            height: "24px",
+            borderRadius: "4px",
+            background: "rgb(64, 112, 224)",
+          }}
+        />
+        <div className="interval-container">
+          <span />
+          <span />
+          <span />
+          <span />
+        </div>
+      </div>
     </div>
   );
 }
